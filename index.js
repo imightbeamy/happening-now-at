c5c05184880764be1fc286d206c57d6c9d17ac6f
Amy Ciavolino <amyciav@gmail.com>
2014-03-13 14:27:54 -0700

    clean up a bit

--- index.js
+++ index.js
@@ -5,36 +5,35 @@ var _ = require('underscore');
 var mustache = require('mustache');
 var moment = require('moment');
 var fs = require('fs'); 
-
 var express = require("express");
 var logfmt = require("logfmt");
-var app = express();
+
+function isComingUp(now) {
+  return function(e) {
+    if (e.start && e.end) {
+      var start_diff = now.diff(e.start.getTime(), 'minute'),
+      end_diff   = now.diff(e.end.getTime(), 'minute');
+      return  start_diff < 30 && start_diff > -60 && end_diff < -5;
+    }
+    return false;
+  }
+}
 
 function timeId(e) {
   return moment(e.start).format('LT') + moment(e.end).format('LT');
 }
 
-app.use(logfmt.requestLogger());
+var tpl = fs.readFileSync("schedule.mustache", "utf8"),
+    all_events = ical.parseFile('schedule.ics'),
+    app = express();
 
+app.use(logfmt.requestLogger());
 app.get('/', function(req, res) {
 
   var now = moment().zone(7);
-  var tpl = fs.readFileSync("schedule.mustache", "utf8");
-//  var now = moment([2014,2,13,11,36]).zone(7);
-
-  function isComingUp(e) {
-    if (e.start && e.end) {
-      var start_diff = now.diff(e.start.getTime(), 'minute'),
-      end_diff   = now.diff(e.end.getTime(), 'minute');
-      return  start_diff < 30 && start_diff > -60 && end_diff < -10;
-    }
-    return false;
-  }
-
-  events = ical.parseFile('schedule.ics');
-  events = _.filter(events, isComingUp);
+  events = _.filter(all_events, isComingUp(now));
   events = _.sortBy(events, 'start');
-  times = _.uniq(_.map(events, timeId));
+  times  = _.uniq(_.map(events, timeId));
   events = _.map(events, function(e) {
     return {
       start_time: moment(e.start).format('LT'),
@@ -47,9 +46,12 @@ app.get('/', function(req, res) {
     }
   });
 
-  var html = mustache.to_html(tpl, { events: events, current_time: now.format('LT')});
+  var html = mustache.to_html(tpl, {
+    events: events, 
+    current_time: now.format('LT')
+  });
+
   res.send(html);
-  
 });
 
 var port = Number(process.env.PORT || 5000);
