d2a6fe3a044cfce73cc9357abfa44150dba8cc82
Amy Ciavolino <amyciav@gmail.com>
2014-03-13 00:28:21 -0700

    Fix time zone

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
 
-  var now = moment().zone(8);
+  var now = moment().zone(-8);
 
   function isComingUp(e) {
     if (e.start && e.end) {
